d8a76d2fc71300e27ad3bb9fe81fff0a10696193
sunnyleeyun <sunnylee945@hotmail.com>
2020-12-08 00:38:44 +0800

    Add "Animating Views and Transitions"

---
+++ Landmarks/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by 李昀 on 2020/12/7.
+//
+
+import SwiftUI
+
+struct Hike: Codable, Hashable, Identifiable {
+  var name: String
+  var id: Int
+  var distance: Double
+  var difficulty: Int
+  var observations: [Observation]
+  
+  static var formatter = LengthFormatter()
+  
+  var distanceText: String {
+    return Hike.formatter.string(fromValue: distance, unit: .kilometer)
+  }
+  
+  struct Observation: Codable, Hashable {
+    var distanceFromStart: Double
+    
+    var elevation: Range<Double>
+    var pace: Range<Double>
+    var heartRate: Range<Double>
+  }
+}
